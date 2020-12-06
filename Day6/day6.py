2e3c4ff68b4bd68f0bdaf27156bc95e81dd07f88
ellendmk <ellendemellokoch@gmail.com>
2020-12-06 07:58:15 +0200

    adding day 6

---
+++ Day6/day6.py
@@ -0,0 +1,25 @@
+f = open('input.txt', 'r')
+
+contents = f.read().split('\n\n')
+
+counts = 0
+common_count = 0
+
+for i in contents:
+    ans = []
+    text = (i.split('\n'))
+    text_joined=''.join(text)
+    items = set(text_joined)
+    check = 0
+    for k in items:
+        check=0
+        for n in text:
+            if k in n:
+                check+=1
+        if check==len(text):
+            common_count+=1
+
+    counts+=len(set(text_joined))
+print('Part 1:',counts)
+print('Part 2:',common_count)
+
